9793d39f37fe5b7e07f0e32a1c82f7d4b015c1d8
aldo.larrain@hotmail.com <aldo.larrain@hotmail.com>
2020-03-07 11:54:30 -0400

    2do cambio

--- webapiCalculadora/utCalculadora/UnitTestCalculadora.cs
+++ webapiCalculadora/utCalculadora/UnitTestCalculadora.cs
@@ -21,5 +21,69 @@ namespace utCalculadora
             //assert=verificacion
             Assert.AreEqual(numres, result);
         }
+        [TestMethod]
+        public void TestAddGet2()
+        {
+            //Arrange= preparacion 
+            CalculatorController calc = new CalculatorController();
+            double numas = 0.3;
+            double numbs = 0.4;
+            double numress = 0.7;
+
+            //Act=ejecucion
+            double result = calc.Adddec(numas, numbs);
+
+            //assert=verificacion
+            Assert.AreEqual(numress, result);
+        }
+        [TestMethod]
+        public void TestAdd3Get2()
+        {
+            //Arrange= preparacion 
+            CalculatorController calc = new CalculatorController();
+           int numa = -3;
+            int numb = -4;
+            int numc = -5;
+            int numres = -12;
+
+            //Act=ejecucion
+            int result = calc.Add3(numa, numb,numc,numc);
+
+            //assert=verificacion
+            Assert.AreEqual(numres, result);
+        }
+        [TestMethod]
+        public void TestMultGet1()
+        {
+            //Arrange= preparacion 
+            CalculatorController calc = new CalculatorController();
+            int numa = 3;
+            int numb = 4;
+            int numc = 2;
+            int numres = 24;
+
+            //Act=ejecucion
+            double result = calc.Mult3(numa, numb, numc);
+
+            //assert=verificacion
+            Assert.AreEqual(numres, result);
+        }
+        [TestMethod]
+        public void TestMult3Get2()
+        {
+            //Arrange= preparacion 
+            CalculatorController calc = new CalculatorController();
+            int numa = -3;
+            int numb = -4;
+            int numc = -2;
+            int numres = -24;
+
+            //Act=ejecucion
+            double result = calc.Mult3(numa, numb, numc);
+
+            //assert=verificacion
+            Assert.AreEqual(numres, result);
+        }
+
     }
 }
